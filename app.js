d6e04fb6e714e5acf77a9478b9b72ed0dafc7bc1
okan87 <okankaplan1907@gmail.com>
2024-08-19 13:13:31 +0200

    income section is created

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 //!Selectors
+
+//*Income Selectors
+const addForm = document.getElementById("add-form");
+const incomeInput = document.getElementById("income-input");
+const addButon = document.getElementById("add*btn");
+const yourIncome = document.getElementById("your-income")
 //!Variables
+let gelirler = "";
+
 //!Events
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  gelirler = Number(gelirler) + Number(incomeInput.value);
+  localStorage.setItem("gelirler", gelirler);
+  console.log(gelirler);  
+  addForm.reset();
+  calculateAndUpdate()
+});
+
+window.addEventListener("load", ()=>{
+    gelirler = Number(localStorage.getItem("gelirler"))
+    calculateAndUpdate()
+})
+
 //!Functions
+const calculateAndUpdate = ()=>{
+yourIncome.innerText = gelirler
+}
